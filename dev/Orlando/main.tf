75a46b5ebd93fb532aab85944f90ca742c1f29e8
Michael Godfrey <migodfre@microsoft.com>
2024-06-03 14:27:08 -0400

    updated main.tf in Orlando

--- dev/Orlando/main.tf
+++ dev/Orlando/main.tf
@@ -7,37 +7,29 @@
    endingAddress   = "10.50.10.29"
    defaultGateway  = "10.50.10.1.1"
    dnsServers      = ["10.254.0.196", "10.254.0.197"]
-   adouSuffix      = "<adouSuffix>"
-   domainServerIP  = "<domainServerIP>"
+   adouSuffix      = "OU=Hypervisors,OU=Servers,OU=Computers,OU=adaptivecloudlab,DC=adaptivecloudlab,DC=com"
+   domainServerIP  = "10.254.0.196"
    servers = [
     {
-       name        = "AZ-Node1",
-       ipv4Address = "172.25.117.11"
+       name        = "Orl-Node1",
+       ipv4Address = "10.50.10.11"
      },
      {
-       name        = "AZ-Node2",
-       ipv4Address = "172.25.117.13"
-     },
-      {
-       name        = "AZ-Node3",
-       ipv4Address = "172.25.117.15"
-     },
-      {
-       name        = "AZ-Node4",
-       ipv4Address = "172.25.117.17"
+       name        = "Orl-Node2",
+       ipv4Address = "10.50.10.12"
      }
    ]
-    managementAdapters = ["Port1", "Port0"]
+    managementAdapters = ["Port0", "Port1"]
    storageNetworks = [
      {
        name               = "Storage1Network",
        networkAdapterName = "Port0",
-       vlanId             = "21"
+       vlanId             = "711"
      },
      {
        name               = "Storage2Network",
        networkAdapterName = "Port1",
-       vlanId             = "22"
+       vlanId             = "712"
      }
    ]
    rdmaEnabled                   = true     // Change to true if RDMA is enabled.
@@ -55,14 +47,14 @@
    rpServicePrincipalObjectId    = var.rpServicePrincipalObjectId
 
    # Region HCI logical network parameters
-   lnet-startingAddress = "172.25.117.200"
-   lnet-endingAddress   = "172.25.117.220"   //This IP range should not overlap with HCI infra IP range.
-   lnet-addressPrefix   = "172.25.117.0/24"   //E.g., 192.168.1.0/24
-   lnet-defaultGateway  = "172.25.117.1"  //Default gateway can be same as HCI infra default gateway.
+   lnet-startingAddress = "10.20.242.20"
+   lnet-endingAddress   = "10.20.242.100"   //This IP range should not overlap with HCI infra IP range.
+   lnet-addressPrefix   = "10.20.242.0/24"   //E.g., 192.168.1.0/24
+   lnet-defaultGateway  = "10.20.212.1"  //Default gateway can be same as HCI infra default gateway.
    lnet-dnsServers      = ["10.254.0.196", "10.254.0.197"]   // DNS servers can be same as HCI infra DNS servers.
 
    # Region AKS Arc parameters
-   aksArc-controlPlaneIp   = "172.25.117.221"       //An IP address in the logical network IP range.
+   aksArc-controlPlaneIp   = "10.20.242.110"       //An IP address in the logical network IP range.
    rbacAdminGroupObjectIds = ["be0c17dc-9a37-48c5-9691-751a27a4c1b9", "f5157bd2-8ce4-48b6-82df-69b9de7540a9", "904e7142-bfcf-4071-a326-6d798140dd03"] // An AAD group that will have the admin permission of this AKS Arc cluster. Check ./doc/AKS-Arc-Admin-Groups.md for details
 
   #  Region HCI VM parameters
